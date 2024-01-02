3c05a5d5cc8cfdc60387bd188263fce212162fd6
Lara Cunha <up202108876@up.pt>
2024-01-02 17:21:21 +0000

    Folha1

--- Haskell/Folha1.hs
+++ Haskell/Folha1.hs
@@ -17,7 +17,7 @@ Ex 1.14.a
 curta :: [a] -> Bool
 curta xs = length xs <= 2
 
-Ex1.14.b -}
+Ex1.14.b 
 
 
 curta :: [a] -> Bool
@@ -25,4 +25,46 @@ curta []     = True   -- Lista vazia, considerada curta
 curta [_]    = True   -- Lista com um elemento, considerada curta
 curta [_,_]  = True   -- Lista com dois elementos, considerada curta
 curta _      = False  -- Outros casos, considerada não curta
+
+
+-- 1.1
+testaTriangulo :: Float -> Float -> Float -> Bool
+testaTriangulo a b c = a < b + c && b < a + c && c < a + b
+
+-- 1.2
+areaTriangulo :: Float -> Float -> Float -> Float
+areaTriangulo a b c =
+  let s = (a + b + c) / 2  -- Semiperímetro
+      area = sqrt (s * (s - a) * (s - b) * (s - c))
+  in area
+
+-- 1.3
+metades :: [a] -> ([a], [a])
+metades lista =
+  let len = length lista
+      metade = len `div` 2
+      primeiraMetade = take metade lista
+      segundaMetade = drop metade lista
+  in (primeiraMetade, segundaMetade)
+
+
+-- 1.4.a)
+
+last' :: [a] -> a
+last' = head . reverse
+
+last'' :: [a] -> a
+last'' xs = head (drop (length xs - 1) xs)
+
+-- 1.4.b)
+
+init' :: [a] -> [a]
+init' = reverse . tail . reverse
+
+
+init'' :: [a] -> [a]
+init'' xs = take (length xs - 1) xs
+
+-}
+
 
